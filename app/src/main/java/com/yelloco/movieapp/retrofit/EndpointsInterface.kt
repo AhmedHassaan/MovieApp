fd13692be2bea914b9ce5f4f6487e0b5a442a7ea
Ahmed Hassaan <ahmed.hassaan@yelloco.com>
2021-03-14 22:33:25 +0200

    sample url

--- app/src/main/java/com/yelloco/movieapp/retrofit/EndpointsInterface.kt
+++ app/src/main/java/com/yelloco/movieapp/retrofit/EndpointsInterface.kt
@@ -8,6 +8,8 @@ import retrofit2.http.Query
 
 interface EndpointsInterface {
 
+    // https://api.themoviedb.org/3/movie/now_playing?api_key=9f8f4a4070f14920238abc2d569f283b&language=en-US
+
     private val lang : String
         get() = "en-US"
 
